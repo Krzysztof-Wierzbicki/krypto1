24f28414e9fe7302be2d198cf974a29ea6d57e04
Karol Podlewski <karol.podlewski@hotmail.com>
2018-10-26 13:48:44 +0200

    Merged files to one

--- include/DesTables.h
+++ include/DesTables.h
@@ -1,7 +1,7 @@
-#ifndef DES_PERMUTATION_H
-#define DES_PERMUTATION_H
+#ifndef DES_TABLES_H
+#define DES_TABLES_H
 
-// Initial permutation table [8*8]
+// Initial permutation
 static const char IP[] =
 {
     58, 50, 42, 34, 26, 18, 10, 2,
@@ -14,20 +14,21 @@ static const char IP[] =
     63, 55, 47, 39, 31, 23, 15, 7
 };
 
-// Final permutation table [8*8]
-static const char FP[] =
+// Permuted Choice 1
+static const char PC1[] =
 {
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-    37, 5, 45, 13, 53, 21, 61, 29,
-    36, 4, 44, 12, 52, 20, 60, 28,
-    35, 3, 43, 11, 51, 19, 59, 27,
-    34, 2, 42, 10, 50, 18, 58, 26,
-    33, 1, 41,  9, 49, 17, 57, 25
+    57, 49, 41, 33, 25, 17,  9,
+     1, 58, 50, 42, 34, 26, 18,
+    10,  2, 59, 51, 43, 35, 27,
+    19, 11,  3, 60, 52, 44, 36,
+
+    63, 55, 47, 39, 31, 23, 15,
+     7, 62, 54, 46, 38, 30, 22,
+    14,  6, 61, 53, 45, 37, 29,
+    21, 13,  5, 28, 20, 12,  4
 };
 
-// Expansion table [6*8]
+// Expansion function
 static const char E[] =
 {
     32,  1,  2,  3,  4,  5,
@@ -40,8 +41,28 @@ static const char E[] =
     28, 29, 30, 31, 32,  1
 };
 
-// The S-Box tables [8*16*4]
-static const char SBOX[8][64] =
+// Iteration Shift Array
+static const char ITERATION_SHIFT[] =
+{
+//  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16
+    1,  1,  2,  2,  2,  2,  2,  2,  1,  2,  2,  2,  2,  2,  2,  1
+};
+
+// Permuted Choice 2
+static const char PC2[] =
+{
+    14, 17, 11, 24,  1,  5,
+     3, 28, 15,  6, 21, 10,
+    23, 19, 12,  4, 26,  8,
+    16,  7, 27, 20, 13,  2,
+    41, 52, 31, 37, 47, 55,
+    30, 40, 51, 45, 33, 48,
+    44, 49, 39, 56, 34, 53,
+    46, 42, 50, 36, 29, 32
+};
+
+// S-Box
+static const char SBOX[][64] =
 {
     {
         // S1
@@ -101,17 +122,26 @@ static const char SBOX[8][64] =
     }
 };
 
-// Post S-Box permutation [4*8]
-static const char PBOX[] =
+// Post S-Box permutation
+static const char PSBOX[] =
 {
-    16,  7, 20, 21,
-    29, 12, 28, 17,
-     1, 15, 23, 26,
-     5, 18, 31, 10,
-     2,  8, 24, 14,
-    32, 27,  3,  9,
-    19, 13, 30,  6,
-    22, 11,  4, 25
+    16,  7, 20, 21, 29, 12, 28, 17,
+     1, 15, 23, 26,  5, 18, 31, 10,
+     2,  8, 24, 14, 32, 27,  3,  9,
+    19, 13, 30,  6, 22, 11,  4, 25
 };
 
-#endif // DES_PERMUTATION_H
+// Final permutation
+static const char FP[] =
+{
+    40, 8, 48, 16, 56, 24, 64, 32,
+    39, 7, 47, 15, 55, 23, 63, 31,
+    38, 6, 46, 14, 54, 22, 62, 30,
+    37, 5, 45, 13, 53, 21, 61, 29,
+    36, 4, 44, 12, 52, 20, 60, 28,
+    35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26,
+    33, 1, 41,  9, 49, 17, 57, 25
+};
+
+#endif // DES_TABLES_H
